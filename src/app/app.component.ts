c0d8cc731aa3695465633bd056486f66b1c9c5bf
uragun91 <f.vanya91@gmail.com>
2020-04-29 00:15:46 +0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent {
+  public items: any[]
+
+  constructor(private http: HttpClient) {
+
+  }
+  
   name = 'Angular';
 }
