7c2ac05aa09661c67ceed1cbb0f03f66e4f50450
uragun91 <f.vanya91@gmail.com>
2020-04-29 00:17:59 +0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   public items: any[]
 
   constructor(private http: HttpClient) {
-
+    this.http.get('http://localhost:3000/products/index/1')
   }
   
   name = 'Angular';
